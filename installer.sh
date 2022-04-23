c5cc97eeaac9d5f8a3361f914be6993ee52e339c
tar1971 <96209005+tar1971@users.noreply.github.com>
2022-04-23 22:43:03 +0200

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
  # 
 #command wget https://raw.githubusercontent.com/tar1971/plugin/main/installer.sh -O - | /bin/sh
- # # 
-cd /tmp
-set -e 
+ #!/bin/sh
+ # 
 install --force-overwrite "https://github.com/tar1971/plugin/blob/main/enigma2-plugin-extensions-xcplugin-forever_1.8_all.ipk?raw=true"
 wait
 exit 0
