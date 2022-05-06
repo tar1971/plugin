a5de50c20e777f2b65db76c3c320af8d1b54ff09
tar1971 <96209005+tar1971@users.noreply.github.com>
2022-05-06 23:46:33 +0200

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 ######################################################################################
-## Command=wget https://raw.githubusercontent.com/tar1971/feeds-finder/main/installer.sh -O - | /bin/sh
+## Command=wget https://raw.githubusercontent.com/tar1971/plugin/main/installer.sh -O - | /bin/sh
 ##
 ###########################################
 ###########################################
 #!/bin/sh
 echo
 
-opkg install --force-overwrite  https://github.com/tar1971/feeds-finder/blob/main/feeds-finder_V1.6_py2_all.ipk?raw=true
+opkg install --force-overwrite  https://github.com/tar1971/plugin/blob/main/enigma2-plugin-extensions-xcplugin-forever_1.8_all.ipk?raw=true
 sleep 2;
 exit 0
 
