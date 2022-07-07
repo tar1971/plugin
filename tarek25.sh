93ec89d59248ae3a2652283ff2fad1a72ee74b77
tar1971 <96209005+tar1971@users.noreply.github.com>
2022-07-07 11:37:19 +0200

    Update tarek25.sh

--- tarek25.sh
+++ tarek25.sh
@@ -7,7 +7,7 @@
 #!/bin/sh
 #
 wget -O /tmp/EPG-PRO-grabber.tar.gz "https://drive.google.com/uc?id=15TJ9-_rnywMmJhoPFiu_9rLY6fNxtQ0O&export=download"
-
+wait
 tar -xzf /tmp/EPG-PRO-grabber.tar.gz -C /
 
 rm -r /tmp/EPG-PRO-grabber.tar.gz
@@ -15,27 +15,3 @@ rm -r /tmp/EPG-PRO-grabber.tar.gz
 killall -9 enigma2
 
 sleep 2;
-exit 0
-###########################################
-###########################################
-echo ""
-# Download and install plugin
-cd /tmp
-set -e
-     wget "$MY_URL/$MY_IPK"
-  wait
-     wget "$MY_URL/$MY_DEB"
-
- if which dpkg > /dev/null 2>&1; then
-		dpkg -i --force-overwrite $MY_DEB; apt-get install -f -y
-	else
-		$OPKGINSTALL $MY_IPK
-	fi
-echo "================================="
-set +e
-cd ..
-wait
-rm -f /tmp/$MY_IPK
-rm -f /tmp/$MY_DEB
-	if [ $? -eq 0 ]; then
-echo ">>>>  SUCCESSFULLY INSTALLED <<<<"
