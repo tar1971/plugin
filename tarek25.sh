e383672428a60cf4a5c2b432f50ecd127379f569
tar1971 <96209005+tar1971@users.noreply.github.com>
2022-07-07 10:32:49 +0200

    Add files via upload

---
+++ tarek25.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+######################################################################################
+## Command=wget https://raw.githubusercontent.com/tar1971/plugin/main/tarek25.sh -O - | /bin/sh
+##
+###########################################
+###########################################
+#!/bin/sh
+#
+
+wget -O /tmp/EPG-PRO-grabber.tar.gz  "https://onedrive.live.com/download?cid=CFCA224FBA296C58&resid=CFCA224FBA296C58%21176&authkey=AHeflovfFcgv4UQ"
+
+tar -xzf /tmp/EPG-PRO-grabber.tar.gz -C /
+
+rm -r /tmp/EPG-PRO-grabber.tar.gz
+
+killall -9 enigma2
+
+sleep 2;
+###########################################
+###########################################
+echo ""
+# Download and install plugin
+cd /tmp
+set -e
+     wget "$MY_URL/$MY_IPK"
+  wait
+     wget "$MY_URL/$MY_DEB"
+
+ if which dpkg > /dev/null 2>&1; then
+		dpkg -i --force-overwrite $MY_DEB; apt-get install -f -y
+	else
+		$OPKGINSTALL $MY_IPK
+	fi
+echo "================================="
+set +e
+cd ..
+wait
+rm -f /tmp/$MY_IPK
+rm -f /tmp/$MY_DEB
+	if [ $? -eq 0 ]; then
+echo ">>>>  SUCCESSFULLY INSTALLED <<<<"
