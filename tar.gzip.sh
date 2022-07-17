c828fe181bb5c9bd2a5e0507536bce791b1050b9
tar1971 <96209005+tar1971@users.noreply.github.com>
2022-07-18 01:31:41 +0200

    Update tar.gzip.sh

--- tar.gzip.sh
+++ tar.gzip.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
-
 wget https://raw.githubusercontent.com/tar1971/plugin/main/tar.gzip.sh -O - | /bin/sh
 ##
 ###########################################
 ###########################################
 #!/bin/sh
 echo
-
 wget 
 "https://github.com/tar1971/plugin/blob/main/script-a.tar.gz?raw=true"
 
