c916d67e4e8e4fc2a08ef4142c2025850cd2622c
tar1971 <96209005+tar1971@users.noreply.github.com>
2022-07-18 00:56:07 +0200

    Add files via upload

---
+++ tar.gzip.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+######################################################################################
+## Command=wget https://raw.githubusercontent.com/tar1971/plugin/main/tar.gzip.sh -O - | /bin/sh
+##
+###########################################
+###########################################
+
+
+wait
+
+
+wget "https://raw.githubusercontent.com/tar1971/plugin/main/script-a.tar.gz"
+
+
+tar -xzf script-a.tar.gz  -C /
+
+wait
+rm -f /tmp/script-a.tar.gz
+echo "   UPLOADED BY  >>>>   TAREK_TT "   
+sleep 4;                                                                                                                  
+echo ". >>>>         RESTARING     <<<<"
+echo "**********************************************************************************"
+wait
+killall -9 enigma2
+exit 0
+
+
+
+
+
+
+
+
+
+
+
+
+
+
