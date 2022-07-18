da9f72ec8ee2b71dc83852b982e8b810b4c7529c
tar1971 <96209005+tar1971@users.noreply.github.com>
2022-07-18 02:03:36 +0200

    Update tar.gzip.sh

--- tar.gzip.sh
+++ tar.gzip.sh
@@ -4,82 +4,23 @@
 ###########################################
 ###########################################
 #!/bin/sh
-#
+ # 
+echo
+# script download my scripts #  
+cd /tmp
+set -e 
+wget -O /tmp/script-a.tar.gz "https://drive.google.com/uc?id=1gEgP9Wsem6nvZCjkrrTkX78d_Dal951w&export=download" > /tmp/script-a.tar.gz
+sleep 1
 wait
-
-wget -O /tmp/script-a.tar.gz "https://github.com/tar1971/plugin/blob/main/script-a.tar.gz?raw=true" > /tmp/script-a.tar.gz
-
-tar -xzf /tmp/script-a.tar.gz -C /
-
+tar -xzf script-a.tar.gz  -C /
 wait
-
-rm -r /tmp/script-a.tar.gz
-
+cd ..
+set +e
+rm -f /tmp/script-a.tar.gz
+echo "   UPLOADED BY  >>>>   TAR_TAR "   
+sleep 4;                                                                                                                  
+echo ". >>>>         RESTARING     <<<<"
+echo "**********************************************************************************"
 wait
-
 killall -9 enigma2
-
-sleep 2;
-
 exit 0
-
-
-
-
-
-
-MY_MAIN_URL="https://raw.githubusercontent.com/tar1971/EPG-PRO/"
-MY_URL=$MY_MAIN_URL$PACKAGE_DIR'/'$MY_FILE
-MY_TMP_FILE="/tmp/"$MY_FILE
-
-rm -f $MY_TMP_FILE > /dev/null 2>&1
-
-MY_SEP='============================================================='
-echo $MY_SEP
-echo 'Downloading '$MY_FILE' ...'
-echo $MY_SEP
-echo ''
-wget -T 2 $MY_URL -P "/tmp/"
-
-if [ -f $MY_TMP_FILE ]; then
-
-	echo ''
-	echo $MY_SEP
-	echo 'Extracting ...'
-	echo $MY_SEP
-	echo ''
-	tar -xf $MY_TMP_FILE -C /
-	MY_RESULT=$?
-
-	rm -f $MY_TMP_FILE > /dev/null 2>&1
-
-	echo ''
-	echo ''
-	if [ $MY_RESULT -eq 0 ]; then
-        echo "#########################################################"
-        echo "#  NitroAdvanceFHD Skin $version INSTALLED SUCCESSFULLY      #"
-        echo "#                BY BIKO - support on                   #"
-        echo "#   https://www.tunisia-sat.com/forums/forums/182/      #"
-        echo "#########################################################"
-        echo "#           your Device will RESTART Now                #"
-        echo "#########################################################"		
-		if which systemctl > /dev/null 2>&1; then
-			sleep 2; systemctl restart enigma2
-		else
-			init 4; sleep 4; init 3;
-		fi
-	else
-		echo "   >>>>   INSTALLATION FAILED !   <<<<"
-	fi;
-	echo ''
-	echo '**************************************************'
-	echo '**                   FINISHED                   **'
-	echo '**************************************************'
-	echo ''
-	exit 0
-else
-	echo ''
-	echo "Download failed !"
-	exit 1
-fi
-# ------------------------------------------------------------------------------------------------------------
